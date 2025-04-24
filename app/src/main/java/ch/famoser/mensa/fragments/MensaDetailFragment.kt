47fc2e088584add91962fd574ba763bebc0bc487
fflopsi <florian.l.frauenfelder@gmail.com>
2025-04-24 14:14:36 +0200

    maint: Remove unused code

--- app/src/main/java/ch/famoser/mensa/fragments/MensaDetailFragment.kt
+++ app/src/main/java/ch/famoser/mensa/fragments/MensaDetailFragment.kt
@@ -10,51 +10,47 @@ import ch.famoser.mensa.compose.MensaDetail
 import ch.famoser.mensa.compose.MensaTheme
 import ch.famoser.mensa.models.Mensa
 import ch.famoser.mensa.repositories.LocationRepository
-import kotlinx.android.synthetic.main.activity_mensa.toolbar_layout
 import java.util.UUID
 
 class MensaDetailFragment : Fragment() {
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private var item: Mensa? = null
+  /**
+   * The dummy content this fragment is presenting.
+   */
+  private var item: Mensa? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        arguments?.let {
-            if (it.containsKey(MENSA_ID)) {
-                val mensaId = UUID.fromString(it.getString(MENSA_ID))
-
-                val locationRepository = LocationRepository.getInstance(requireContext())
-                item = locationRepository.getMensa(mensaId)
-
-                activity?.toolbar_layout?.title = item?.title
-            }
-        }
+    arguments?.let {
+      if (it.containsKey(MENSA_ID)) {
+        val mensaId = UUID.fromString(it.getString(MENSA_ID))
+        val locationRepository = LocationRepository.getInstance(requireContext())
+        item = locationRepository.getMensa(mensaId)
+      }
     }
+  }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-      return ComposeView(requireContext()).apply {
-        setContent {
-          MensaTheme {
-            if (item != null) {
-              MensaDetail(item!!.menus)
-            }
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?,
+  ): View {
+    return ComposeView(requireContext()).apply {
+      setContent {
+        MensaTheme {
+          if (item != null) {
+            MensaDetail(item!!.menus)
           }
         }
       }
     }
+  }
 
-    companion object {
-        /**
-         * The fragment argument representing the item ID that this fragment
-         * represents.
-         */
-        const val MENSA_ID = "item_id"
-    }
+  companion object {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    const val MENSA_ID = "item_id"
+  }
 }
